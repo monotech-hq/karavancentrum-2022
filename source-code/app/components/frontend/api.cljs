a5d950abc2588536e38a305350199288680d42bf
Peter <szan.peter95@gmail.com>
2022-10-11 13:21:30 +0200

    Implemented Ckeditor5.

--- source-code/app/components/frontend/api.cljs
+++ source-code/app/components/frontend/api.cljs
@@ -2,8 +2,11 @@
 (ns app.components.frontend.api
   (:require
     [app.components.frontend.tabs.core :as tabs]
-    [app.components.frontend.sortable.core :as sortable]))
+    [app.components.frontend.sortable.core :as sortable]
+    [app.components.frontend.text-editor.core :as text-editor]))
 
 (def tabs tabs/view)
 
 (def sortable sortable/view)
+
+(def text-editor text-editor/view)
