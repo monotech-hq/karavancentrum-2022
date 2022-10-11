5c08ae198cfe07caad338bdfda4dd616de3bee0d
Peter <szan.peter95@gmail.com>
2022-10-11 13:20:47 +0200

    Added examples to sortable component.

--- source-code/app/components/frontend/sortable/core.cljs
+++ source-code/app/components/frontend/sortable/core.cljs
@@ -57,10 +57,10 @@
 ;; ---- Components ----
 
 (defn sortable-item [{:keys [index id] :as props} item]
-  (let [sortable (to-clj-map (useSortable (clj->js {:id id})))
+  (let [sortable (to-clj-map (useSortable (clj->js {:id (str id)})))
         {:keys [attributes listeners setNodeRef isSorting                       ;; pass "attributes" "listeners" to componentn props to make it dragable
                 transform transition isDragging]} sortable]
-    [:div {:key index :data-grabbing isDragging}
+    [:div {:key (str index) :data-grabbing isDragging}
      [:div (merge {:id    (str id)
                    :ref   (js->clj setNodeRef)
                    :style {:transform  (.toString (.-Transform CSS) (clj->js transform))
@@ -95,9 +95,78 @@
         [DragOverlay {:z-index 1} (if @active-id [:div {:style {:cursor "crosshair"}}])]])))
 
 (defn sortable [{:keys [items] :as config}]
+  ; {:items (vector of (string|map))
+  ;  :item  [reagent-component]
+  ;  :item-id-key (keyword)                                                     ;; Needed if items data is map. rendered item id have to be unique
+  ;  :value-path  (vector of keyword)
   (let [items-data (atom items)
         active-id  (atom nil)]
     [:f> dnd-context (assoc config :items items-data :active-id active-id)]))
 
 (defn view [config]
   [sortable config])
+
+;; -----------------------------------------------------------------------------
+;; ---- Examples ----
+;
+;   (defn my-simple-item [{:keys [item-props] :as props}                        ;; item-props is the data from items
+;                         {:keys [attributes listeners] :as sortable-props}])   ;; merge 'attributes' 'listeners' to the element what should handle drag
+;
+;  [components/sortable {:items ["a" "b" "c"]}]
+;                        :value-path [:my-path]
+;                        :item  #'my-simple-item])])
+;
+;  [components/sortable {:items [{:id "id-1" :label "a"}
+;                                 {:id "id-2" :label "b"}
+;                                 {:id "id-3" :label "c"}
+;                        :value-path [:my-path]
+;                        :item-id-key :id])
+;                        :item  #'my-simple-item])])
+;
+
+;; -----------------------------------------------------------------------------
+;; ---- Simple ----
+
+;;
+;; (defn item [{:keys [index item-props] :as props} {:keys [attributes listeners] :as sortable-props}]
+;;   [:<>
+;;    [:div (merge attributes listeners
+;;                {:style {:border "2px solid" :display "flex" :padding "8px"}})
+;;     (str item-props)]])
+;;
+;; (defn- sortable-demo-simple
+;;   []
+;;   [:<> ;[vehicle-images-box]
+;;    [:div
+;;     (let [items @(a/subscribe [:db/get-item [:ide]  ["a" "b" "c" "d" "e"]])]
+;;       [:div {:style {:display "grid" :gap "25px" :grid-template-columns "repeat(auto-fill, minmax(200px, 1fr))"}}
+;;         [components/sortable {:items       items
+;;                               :value-path  [:ide]
+;;                               :item        #'item}]])]])
+;;
+
+;; -----------------------------------------------------------------------------
+;; ---- Drag-handle-example ----
+
+;;  (defn drag-btn [{:keys [attributes listeners isDragging] :as sortable-props}]
+;;    [:div (merge {:class "contents--brand-card-drag-handle"}
+;;                 attributes listeners)
+;;     [elements/icon {:icon :drag_indicator
+;;                     :color (if isDragging :primary :muted)}]])
+;;
+;;  (defn item [{:keys [item-props] :as props} {:keys [attributes listeners] :as sortable-props}]
+;;    [:div {:style {:border "2px solid" :display "flex" :padding "8px"}}
+;;     [drag-btn sortable-props]
+;;     (str (:label item-props))])
+;;
+;;  (defn- sortable-demo
+;;    []
+;;    [:div
+;;      (let [items @(a/subscribe [:db/get-item [:ide]  [{:id "1" :label "a"}
+;;                                                       {:id "2" :label "b"}
+;;                                                       {:id "3" :label "c"}]])]
+;;         ; [:div {:style {:display "grid" :gap "25px" :grid-template-columns "repeat(auto-fill, minmax(200px, 1fr))"}}
+;;          [components/sortable {:items       items
+;;                                :item-id-key :id
+;;                                :value-path  [:ide]
+;;                                :item        #'item}])]])
